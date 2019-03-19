09bf621bdbc19f12e983feaa230e0b87c7b43e72
Wyeth Jackson <wyeth@gotab.io>
2019-03-19 11:26:37 -0400

    config

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 require("marko/node-require");
-const express = require('express');
-const markoExpress = require("marko/express");
-const fs = require("fs");
-const app = express();
-const hello = require("./hello");
-const layout = require("./layout");
-const index = require("./index.marko");
-const path = require('path');
+require('dotenv').config()
 require('lasso').configure({
     "plugins": [
         "lasso-marko"
     ],
 });
+const express = require('express');
+const markoExpress = require("marko/express");
+const fs = require("fs");
+const app = express();
+const layout = require("./layout");
+const index = require("./index.marko");
+const path = require('path');
+const PORT = process.env.PORT;
 app.use(express.static('public'));
 app.use(require('lasso/middleware').serveStatic());
 app.get('/', function (req, res) {
-  // let the browser know html is coming
-res.setHeader("content-type", "text/html");
-
-// render the output to the `res` output stream
- res.marko(index, {name: "markooooo"});
+  res.setHeader("content-type", "text/html");
+  res.marko(index, {});
 })
 
-app.listen(5000, function () {
-  console.log("YOjfeisofjeirjsf")
+app.listen((PORT || 5000), function () {
   if(process.send) {
     process.send('online');
   }
